bdb0c1a1dffc2c53d00a828129ac211a89b7780c
Myck William dos Santos <mick.santos@sergipetec.org.br>
2022-10-14 22:21:13 -0300

    Project start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { DefaultComponent } from './layouts/default/default.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostsComponent } from './modules/posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DefaultComponent,
+    children: [
+      { path: '', component: DashboardComponent },
+      { path: 'posts', component: PostsComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
