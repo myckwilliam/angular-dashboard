ef955db14f068d4bb455f5474307ded3f33eb5f9
Myck William dos Santos <mick.santos@sergipetec.org.br>
2022-10-14 22:29:58 -0300

    Add Sidenav

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -5,9 +5,10 @@ import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.componen
 import { RouterModule } from '@angular/router';
 import { PostsComponent } from 'src/app/modules/posts/posts.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [DefaultComponent, DashboardComponent, PostsComponent],
-  imports: [CommonModule, RouterModule, SharedModule],
+  imports: [CommonModule, RouterModule, SharedModule, MatSidenavModule],
 })
 export class DefaultModule {}
